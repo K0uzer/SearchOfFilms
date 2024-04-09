57d8eebea53522e9e8712aaba9a86e7f4d01bc06
Alex <87208475+PuSh0chek@users.noreply.github.com>
2024-04-09 23:41:08 +0300

    get first fetch from redux tollkit rtk-query

--- src/Pages/MoviesList.tsx
+++ src/Pages/MoviesList.tsx
@@ -4,8 +4,35 @@ import { Navigate } from 'react-router'
 import SearchAppBar from '../UI/SearchAppBar'
 import FloatingActionButtonExtendedSize from '../UI/FloatingActionButtonExtendedSize'
 import BasicSelect from '../UI/BasicSelect'
-import OutlinedCard from '../UI/OutlinedCard'
-import { Pagination } from '@mui/material'
+import {
+    Button,
+    CardActions,
+    CardContent,
+    Pagination,
+    Typography,
+} from '@mui/material'
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+// const API_URL = 'https://api.kinopoisk.dev/v1.4/movie/12123'
+
+// const api = createApi({
+//     reducerPath: 'api',
+//     baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
+//     endpoints: (builder) => ({
+//         getPokemonByName: builder.query({
+//             query: (movie) => ({
+//                 url: `https://api.kinopoisk.dev/v1.4/${movie}`,
+//                 headers: 'WF76VQQ-HQB4P5G-JFJH8DF-CRKDP1M',
+//             }),
+//         }),
+//     }),
+// })
+// const { useGetFilmByIdQuery } = api
+// console.log(api)
+
+// const getContent = () =>
+// {
+//     fetch('https://api.kinopoisk.dev/v1.4/movie/12123', { headers:{'X-API-KEY': 'WF76VQQ-HQB4P5G-JFJH8DF-CRKDP1M'})
+// }
 
 const MoviesList = () => {
     const { login, password } = useAppSelector((state) => state.movies.user)
@@ -41,8 +68,33 @@ const MoviesList = () => {
                             gridTemplateColumns: '1fr 1fr 1fr 1fr',
                         }}
                     >
-                        <li>
-                            <OutlinedCard />
+                        <li style={{ border: '1px solid black' }}>
+                            <CardContent>
+                                <Typography
+                                    sx={{ fontSize: 14 }}
+                                    color="text.secondary"
+                                    gutterBottom
+                                >
+                                    adw
+                                </Typography>
+                                <Typography variant="h5" component="div">
+                                    awd
+                                </Typography>
+                                <Typography
+                                    sx={{ mb: 1.5 }}
+                                    color="text.secondary"
+                                >
+                                    adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                    well meaning and kindly.
+                                    <br />
+                                    {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
                         </li>
                     </ul>
                 </div>
